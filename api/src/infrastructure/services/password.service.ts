afc668db37a9850f6ea03dd73722cd278e1ed3b2
Kelthon <81057753+kelthon@users.noreply.github.com>
2025-01-23 19:15:10 -0300

    feat: implemented password service

---
+++ api/src/infrastructure/services/password.service.ts
@@ -0,0 +1,14 @@
+import { PasswordService } from '../../domain/interface/password.service';
+import bcrypt from 'bcrypt';
+
+export class BCryptPasswordService implements PasswordService {
+  constructor(private saltRounds: number = 10) {}
+
+  hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, this.saltRounds);
+  }
+
+  comparePasswords(password: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+}
