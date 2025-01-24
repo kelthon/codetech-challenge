a8a91d399c58bdff1c0a6eb570e85f7ea148f890
Kelthon <81057753+kelthon@users.noreply.github.com>
2025-01-23 23:41:09 -0300

    feat: added enum for http status codes

---
+++ api/src/infrastructure/http/http-status-code.ts
@@ -0,0 +1,10 @@
+export enum HTTPStatusCode {
+  OK = 200,
+  Created = 201,
+  NoContent = 204,
+  BadRequest = 400,
+  UnauthorizedError = 401,
+  NotFound = 404,
+  TooManyRequests= 429,
+  InternalServerError = 500,
+}
