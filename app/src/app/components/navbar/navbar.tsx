4df41eb053006d5292ac9fe7fd3951bf9a9c778c
Kelthon <81057753+kelthon@users.noreply.github.com>
2025-01-26 22:09:09 -0300

    feat: created navbar component

---
+++ app/src/app/components/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+import styles from './navbar.module.css';
+
+export function NavBar() {
+  return (
+    <nav className={styles.navbar}>
+      <h1 className={styles.title}>CodeTech</h1>
+    </nav>
+  );
+}
