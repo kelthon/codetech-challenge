91703c160d527e4d7e8967a5a2939c94a8cf4bea
Kelthon <81057753+kelthon@users.noreply.github.com>
2025-01-27 13:14:11 -0300

    feat: created students page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,5 +1,26 @@
+'use client';
+
+import { useRef } from 'react';
+import { NavBar } from './components/navbar/navbar';
+import { SectionHeader } from './components/section-header/section-header';
+import { Table } from './components/table/table';
+
+import styles from './page.module.css';
+
 export default function Home() {
+  const sectionTitleRef = useRef(null);
+
   return (
-    <h1>HomePage</h1>
+    <>
+      <NavBar />
+      <section className={styles.section}>
+        <SectionHeader
+          sectionTitle="Alunos"
+          buttonTitle="Criar Registro"
+          ref={sectionTitleRef}
+        />
+        <Table />
+      </section>
+    </>
   );
 }
