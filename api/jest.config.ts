4d5965cd8c4933461d0d73b81888f33169235323
Kelthon <81057753+kelthon@users.noreply.github.com>
2025-01-27 23:23:15 -0300

    chore: updated tsbuild and jest configs

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -1,9 +1,16 @@
-import type { Config } from "@jest/types";
+import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
   verbose: true,
   automock: true,
-}
+};
 export default config;
