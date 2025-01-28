9c251741617dbfad6a6c9f16e6b83b01eb76065b
Kelthon <81057753+kelthon@users.noreply.github.com>
2025-01-28 02:35:19 -0300

    refactor: moved configurations to config module

--- api/src/infrastructure/database/typeorm.config.ts
+++ api/src/infrastructure/database/typeorm.config.ts
@@ -1,15 +1,16 @@
 import { DataSource } from 'typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { StudentEntity } from '../entities/student.entity';
+import { config } from '../config/config';
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'codetech',
-  synchronize: true,
+  host: config.database.host,
+  port: config.database.port,
+  username: config.database.user,
+  password: config.database.password,
+  database: config.database.name,
+  synchronize: config.database.synchronize,
   logging: false,
   entities: [UserEntity, StudentEntity],
   subscribers: [],
