b5c68dbc33263d4b74e88af61f3354558589354d
Kelthon <81057753+kelthon@users.noreply.github.com>
2025-01-28 02:38:33 -0300

    feat: added new http status code

--- api/src/infrastructure/http/status-code.ts
+++ api/src/infrastructure/http/status-code.ts
@@ -5,6 +5,7 @@ export enum StatusCode {
   NoContent = 204,
   BadRequest = 400,
   UnauthorizedError = 401,
+  Forbidden = 403,
   NotFound = 404,
   TooManyRequests= 429,
   InternalServerError = 500,
