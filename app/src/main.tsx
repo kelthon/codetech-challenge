8ee07fd05ee418157f66f88223f2cc8f5e3aa01d
Kelthon <81057753+kelthon@users.noreply.github.com>
2025-01-28 08:33:42 -0300

    feat: added react-router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,20 @@ import './index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+import HomePage from './pages/home.tsx';
+import RegisterPage from './pages/register.tsx';
+import AuthenticationPage from './pages/auth.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/signin" element={<AuthenticationPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
